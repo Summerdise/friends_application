ae90e9fdea45a427c994489026293e939320d88f
summerdise <350464517@qq.com>
2020-11-16 11:36:16 +0800

    [zmc] feat: first commit and add title bar

---
+++ app/src/main/java/com/example/friendsapplication/TitleLayout.java
@@ -0,0 +1,39 @@
+package com.example.friendsapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TitleLayout extends ConstraintLayout {
+
+
+    @BindView(R.id.title_back_button)
+    ImageButton titleBackBtn;
+    @BindView(R.id.title_photo_button)
+    ImageButton titlePhotoBtn;
+
+
+    public TitleLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        ButterKnife.bind(this);
+        titleBackBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        titlePhotoBtn.setOnClickListener();
+        titlePhotoBtn.setOnLongClickListener();
+    }
+}
